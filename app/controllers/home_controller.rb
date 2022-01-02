a24ffcc6940fce87dc4a3faf21df7a51a9b8c37a
Jordi Martí <jordimarti@me.com>
2022-01-02 22:08:40 +0100

    Dashboard creation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,5 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      @accounts = current_user.accounts
-    else
-      @accounts = nil
-    end
-    if current_account != 0
-      @account_affiliation = AccountAffiliation.find_by(account_id: current_account.id, user_id: current_user.id)
-    else
-      @account_affiliation = nil
-    end
+    @menu_home = true
   end
 end
